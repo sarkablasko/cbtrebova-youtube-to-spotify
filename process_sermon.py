141bee9758907066862fcd6152d39f47f8d91518
sarkadol <sarka.dolezalova123@gmail.com>
2026-09-07 20:48:32 +0200

    zpatky

--- process_sermon.py
+++ process_sermon.py
@@ -12,12 +12,11 @@ REPO_NAME = os.environ.get("GITHUB_REPOSITORY", "sarkablasko/cbtrebova-youtube-t
 
 
 def get_ytdlp_base_cmd():
-    """Zakladni parametry s emulaci mobilnich klientu."""
+    """Zakladni parametry s podporou JS challenge a cookies."""
     cmd = [
         "yt-dlp",
         "--js-runtimes", "node",
-        "--remote-components", "ejs:github",
-        "--extractor-args", "youtube:player_client=ios,mweb"
+        "--remote-components", "ejs:github"
     ]
     if os.path.exists("cookies.txt"):
         cmd.extend(["--cookies", "cookies.txt"])
